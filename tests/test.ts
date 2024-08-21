e8504880c9513e21530310fed4ecc6db0edf471c
Philippe Vinchon <p.vinchon@gmail.com>
2024-08-21 20:10:18 +0100

    Fix tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected board', async ({ page }) => {
+test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.locator('.board')).toBeVisible();
+	await expect(page.locator('h1')).toBeVisible();
 });
