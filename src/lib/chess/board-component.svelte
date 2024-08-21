2b998a508963b305c25bc9090347726980db124c
Philippe Vinchon <p.vinchon@gmail.com>
2024-08-21 19:45:34 +0100

    Display puzzle

---
+++ src/lib/chess/board-component.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Squares } from '$lib/chess/square';
+</script>
+
+<div class="grid grid-cols-8 gap-0.5">
+	{#each Squares.all as square (Squares.getNotation(square))}
+		<slot {square} />
+	{/each}
+</div>
---
+++ src/lib/puzzle/puzzle-component.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+	import { Move } from '$lib/chess/move';
+	import { Pieces } from '$lib/chess/piece';
+	import { type Square, Squares } from '$lib/chess/square';
+	import { ColorPalettes } from '$lib/color-palette';
+	import { Puzzle, Puzzles } from '$lib/puzzle/puzzle';
+
+	import BoardComponent from '$lib/chess/board-component.svelte';
+	import PieceComponent from '$lib/chess/piece-component.svelte';
+	import SquareComponent from '$lib/chess/square-component.svelte';
+
+	export let colorPalette = ColorPalettes.default;
+	export let puzzle: Puzzle;
+	export let onMove: (isCheckmate: boolean) => void;
+
+	var moveOrigin: null | Square = null;
+	var moveDestination: null | Square = null;
+	var moveDestinations: Square[] = [];
+
+	const updateMoveOrigin = (square: Square) => {
+		if (null === moveOrigin || !Squares.isEqual(moveOrigin, square)) {
+			moveOrigin = square;
+			moveDestinations = Puzzles.getValidMoveDestinations(puzzle, square);
+		} else {
+			moveOrigin = null;
+			moveDestinations = [];
+		}
+	};
+
+	const updateMoveDestination = (square: Square) => {
+		if (null === moveOrigin) return;
+
+		moveDestination = square;
+		moveDestinations = [];
+
+		const move = new Move(moveOrigin, square);
+		puzzle = Puzzles.updatePieceSquare(puzzle, move);
+
+		const isMoveCheckmate = Puzzles.isMoveCheckmate(puzzle, move);
+
+		onMove(isMoveCheckmate);
+	};
+</script>
+
+<BoardComponent let:square>
+	<SquareComponent {colorPalette} {square}>
+		{@const piece = Puzzles.getPieceAt(puzzle, square)}
+		{@const isPieceWhite = null !== piece && Pieces.isWhite(piece)}
+		{@const isMoveDestinationValid = moveDestinations.some((validMoveDestination) =>
+			Squares.isEqual(validMoveDestination, square)
+		)}
+
+		{@const onClick = () => {
+			if (null !== moveDestination) return;
+			else if (isPieceWhite) updateMoveOrigin(square);
+			else if (isMoveDestinationValid) updateMoveDestination(square);
+		}}
+
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div
+			class="relative h-full w-full"
+			class:cursor-pointer={null === moveDestination && (isPieceWhite || isMoveDestinationValid)}
+			on:click={onClick}
+		>
+			{#if isMoveDestinationValid}
+				<div
+					class="{colorPalette.squareActive
+						.bg} absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full"
+				></div>
+			{/if}
+
+			{#if null !== piece}
+				<div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+					<PieceComponent {piece} {colorPalette} />
+				</div>
+			{/if}
+		</div>
+	</SquareComponent>
+</BoardComponent>
