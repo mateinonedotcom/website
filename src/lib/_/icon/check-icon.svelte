f2f321e3b8d898dc2082e26b78cec8817a3ef35b
Philippe Vinchon <p.vinchon@gmail.com>
2024-08-23 14:02:30 +0100

    Add settings to configure theme

---
+++ src/lib/_/icon/check-icon.svelte
@@ -0,0 +1,10 @@
+<svg
+	class={$$props.class}
+	fill="none"
+	viewBox="0 0 24 24"
+	stroke-width="5"
+	stroke="currentColor"
+	aria-hidden="true"
+>
+	<path d="M4.5 12.75l6 6 9-13.5"></path>
+</svg>
